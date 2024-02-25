513e52f14de91bc1feac27a5361b0d20b079a6bb
leaps <lmtejedor@gmail.com>
2024-02-24 20:36:46 -0500

    Add openai stuff

--- server.py
+++ server.py
@@ -5,13 +5,22 @@ from typing import List, Optional
 from fastapi.responses import HTMLResponse
 import requests
 import replicate
-
+from openai import OpenAI
 import matplotlib.pyplot as plt
 import numpy as np
 import random
-
+from dotenv import load_dotenv
+import os
 import random
 import json
+
+load_dotenv()
+
+client = OpenAI(
+  organization=os.getenv("OPENAI_API"),
+)
+
+my_assistant = client.beta.assistants.retrieve("asst_N9nNHYnL43daWRkhoGXIwfJw")
 
 app = FastAPI()
 templates = Jinja2Templates(directory = 'templates')
@@ -25,3 +34,11 @@ def home(request: Request):
     return templates.TemplateResponse('home.html', {
             "request": request,
         })
+
+class Item(BaseModel):
+    search: str
+
+@app.post("/")
+async def root(item: Item):
+    thread = client.beta.threads.create()
+    print(thread)
